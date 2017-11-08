d322d0923f13b73005f90e478feb88b1d66f00cf
Pavol Kiss <pavol.kiss@akademiasovy.com>
2017-11-08 12:26:24 +0100

    pocitadlo

--- pocitadlo.c
+++ pocitadlo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+									//urobit maximalne 3 pokusy
 int main(int argc, char *argv[]) {
 	char answer[30];
 		
@@ -9,13 +9,17 @@ int main(int argc, char *argv[]) {
 	scanf("%s", &answer);
 	int len=strlen(answer);
 	int i;
+	//int pokus;
+	for(answer=1; answer<=3; i++){
+		
+	}
 	for(i=0;i<len;i++)
 	answer[i]=tolower(answer[i]); 	//zmeni tolower na male pismena
 	if(strcmp(answer,"bratislava")==0)
 	{
 		printf("\nCorrect answer");
 	}
-	
+			
 	else{
 		printf("Wrong");
 	}
